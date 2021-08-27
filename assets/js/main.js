66abe9ad94ad3cbcfeb28dc3a0233c65f09fd5f5
Camilo Andres Navarrete Ruiz <Camilo.Navarrete@iq-online.com>
2021-08-26 23:34:17 -0500

    Se actualiza HV actualizando perfil profesional y cargando nuevos certificados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 var years = "2012-08-03";
 var parraf01 = "Ingeniero electrónico, con experiencia en el sector tecnológico por más de";
-var parraf02 = "años como desarrollador de software full stack, con conocimientos en lenguaje C# utilizando el framework .Net para el desarrollo y mantenimiento de sitios web, de aplicaciones de escritorio, servicios Windows y servicios web (web api, wcf), desarrollo de sitios web con framework frontend basados en componentes como (Angular, Angular js y React js) ejecutados sobre Node.js, conocimientos en bases de datos de tipo SQL server y Oracle, desarrollo de sitios web con Aps.net MVC, lenguaje de etiquetas html y hojas de estilos css aplicando responsive design con bootstrap o foundation, conocimientos en arquitectura javascript, manejo de software para el control de código como Git y Visual studio team services, conocimientos en desarrollo ágil utilizando (scrum), creación y administración de aplicaciones en la nube con Azure, conocimientos en el desarrollo e implementación del protocolo FIX.";
-var parraf03 = "Dentro de mis habilidades resalto la busqueda de soluciones efectivas, rapidas y de alto impacto para evitar las consecuencias del error, tambien resalto mi disposición para el trabajo en equipo, mis cualidades y valores más resaltantes son la honestidad, la puntualidad responsabilidad, emprendimiento y paciencia, me gusta dar mi punto de vista para ser proactivo en el trabajo.";
+var parraf02 = "años como desarrollador de software full stack, con conocimientos en lenguaje C# utilizando el framework .Net y .Net Core para el desarrollo y mantenimiento de sitios web, aplicaciones de escritorio, servicios Windows y servicios web (web api, wcf), desarrollo de sitios web con framework frontend basados en componentes como (Angular, Angular js y React js) ejecutados sobre Node.js, conocimientos en bases de datos de tipo SQL server, desarrollo de sitios web con Aps.net MVC, lenguaje de etiquetas html y hojas de estilos css aplicando responsive design con bootstrap o foundation, conocimientos en arquitectura javascript, manejo de software para el control de código como Git y Visual studio team services, conocimientos en desarrollo ágil usando (scrum), experiencia con integración y despliegue continuo en la plataforma azure devops, creación y administración de aplicaciones en la nube con Azure, conocimientos en el desarrollo e implementación del protocolo FIX.";
+var parraf03 = "Dentro de mis habilidades resalto la capacidad de liderazgo de equipos de técnicos, mi disposición para el trabajo en equipo, mis cualidades y valores más resaltantes son la honestidad, puntualidad, responsabilidad, proactividad y paciencia.";
 
 jQuery(document).ready(function($) {
     /*======= Skillset *=======*/
     years = getAge(years);
-    $('.Parrafo1').append(parraf01 +" "+years+" "+parraf02);
+    $('.Parrafo1').append(`${parraf01} ${years} ${parraf02}`);
     $('.Parrafo2').append(parraf03);
     $('.level-bar-inner').css('width', '0');
     $(window).on('load', function() {
@@ -31,7 +31,7 @@ function getAge(dateString) {
 }
 
 function downloadHV() {
-  var documentDownload = 'HOJADEVIDACAMILONAVARRETE.doc';
+  var documentDownload = 'HOJADEVIDACAMILONAVARRETE.pdf';
   var urlNew = 'assets/documents/' + documentDownload;
   var anchor = document.createElement('a');
 
